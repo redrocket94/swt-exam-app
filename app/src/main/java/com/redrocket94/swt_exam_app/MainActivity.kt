04775ba33d6e87cad1886ad25d586923e61b124a
redrocket94 <oliver.duedam@gmail.com>
2020-05-11 09:37:14 +0200

    Initial commit

---
+++ app/src/main/java/com/redrocket94/swt_exam_app/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.redrocket94.swt_exam_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        nameDoneButton.setOnClickListener {
+            // TODO Check name is viable
+            val intent = Intent(this, QuizActivity::class.java).apply {
+                putExtra("NAME_VAL", nameEditText.text)
+            }
+            startActivity(intent)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/redrocket94/swt_exam_app/QuizActivity.kt
@@ -0,0 +1,12 @@
+package com.redrocket94.swt_exam_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
